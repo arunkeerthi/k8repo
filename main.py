35367be8944e15e45c9d35ba596a134b1b904272
root <root@ip-172-31-3-210.ap-south-1.compute.internal>
2022-03-06 03:30:40 +0000

    third commit

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-#templates = Jinja2Templates(directory="/code")
-templates = Jinja2Templates(directory="C:/Users/mithr/Desktop/k8_project")
+templates = Jinja2Templates(directory="/code")
 
 @app.get("/")
 def form_post(request: Request):
